c08ed53d90e204d1d2eb2407b449e3e1b17432c0
Daniel <besztak.daniel@gmail.com>
2025-10-18 20:23:08 +0200

    [INIT] Initial backend commit

---
+++ linguaai-backend/src/test/java/com/linguaai/LinguaaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linguaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguaaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
