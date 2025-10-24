96e8fb8bf647a5379ae123c4549f49a5b1b3f92e
Daniel <besztak.daniel@gmail.com>
2025-10-24 18:16:56 +0200

    [FEATURE] Add JSON to Entity Converter

---
+++ linguaai-backend/src/main/java/com/linguaai/common/converter/JsonConverter.java
@@ -0,0 +1,34 @@
+package com.linguaai.common.converter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class JsonConverter implements AttributeConverter<Object, String> {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(Object attribute) {
+        if (attribute == null)
+            return null;
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Error converting to JSON", e);
+        }
+    }
+
+    @Override
+    public Object convertToEntityAttribute(String dbData) {
+        if (dbData == null)
+            return null;
+        try {
+            return objectMapper.readValue(dbData, Object.class);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Error converting to JSON", e);
+        }
+    }
+}
