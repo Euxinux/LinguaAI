219b5214341b7e844ff339ab75f4a2f69506a623
Daniel <besztak.daniel@gmail.com>
2025-10-30 21:49:13 +0100

    [FEATURE] Add RoleNotFoundException

---
+++ linguaai-backend/src/main/java/com/linguaai/user/exception/RoleNotFoundException.java
@@ -0,0 +1,13 @@
+package com.linguaai.user.exception;
+
+import lombok.Getter;
+
+@Getter
+public class RoleNotFoundException extends  RuntimeException {
+    private final String roleName;
+
+    public RoleNotFoundException(String roleName) {
+        super("Role with name '" + roleName + "' does not exist.");
+        this.roleName = roleName;
+    }
+}
