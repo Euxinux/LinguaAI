2d665e380346e083d0ff03a588619f59aee11687
Daniel <besztak.daniel@gmail.com>
2025-11-01 00:25:10 +0100

    [FEATURE] Add RoleService class.

---
+++ linguaai-backend/src/main/java/com/linguaai/user/service/RoleService.java
@@ -0,0 +1,33 @@
+package com.linguaai.user.service;
+
+import com.linguaai.user.entity.Role;
+import com.linguaai.user.exception.RoleAlreadyExistsException;
+import com.linguaai.user.repository.RoleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoleService {
+    private final RoleRepository repository;
+
+    public RoleService(RoleRepository roleRepository) {
+        this.repository = roleRepository;
+    }
+
+    public List<Role> getAllRoles() {
+        return repository.findAll();
+    }
+
+    public Optional<Role> getRoleId(String name) {
+        return repository.findByName(name);
+    }
+
+    public Role createNewRole(Role role) {
+        if (repository.findByName(role.getName()).isPresent()) {
+            throw new RoleAlreadyExistsException("Role with name " + role.getName() + " already exists.");
+        }
+        return repository.save(role);
+    }
+}
