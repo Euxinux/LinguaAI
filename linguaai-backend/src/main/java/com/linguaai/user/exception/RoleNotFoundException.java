a7e0b988c7d9ab0f29751bb72410f13689550754
Daniel <besztak.daniel@gmail.com>
2025-11-02 13:13:31 +0100

    [FIX] Change message exception

--- linguaai-backend/src/main/java/com/linguaai/user/exception/RoleNotFoundException.java
+++ linguaai-backend/src/main/java/com/linguaai/user/exception/RoleNotFoundException.java
@@ -3,11 +3,11 @@ package com.linguaai.user.exception;
 import lombok.Getter;
 
 @Getter
-public class RoleNotFoundException extends  RuntimeException {
+public class RoleNotFoundException extends RuntimeException {
     private final String roleName;
 
     public RoleNotFoundException(String roleName) {
-        super("Role with name '" + roleName + "' does not exist.");
+        super("Role with '" + roleName + "' does not exist.");
         this.roleName = roleName;
     }
 }
